145e2fdbabd20450b87c7db1fd7222b61a5b7ece
akshatphumbhra <43840267+akshatphumbhra@users.noreply.github.com>
2020-01-15 22:44:43 +0530

    Restructuring

--- game.py
+++ game.py
@@ -1,179 +1,198 @@
 import pygame
+import random
 
 #Initialize game settings
 def initializeGame():
 
-    pygame.init()
-
-    screen = pygame.display.set_mode((600, 600))
-
-    #Set caption
-    pygame.display.set_caption("Tic Tac Toe")
-
-    #Set Icon
-    #icon = pygame.image.load('image url')
-    #pygame.display.set_icon(icon)
+    global screen, availableGrid, currentPlayer, running
 
     #Set background color
     screen.fill((255,255,255))
-    pygame.draw.line(screen, (0,0,0), (0,200), (600,200))
-    pygame.draw.line(screen, (0,0,0), (0,400), (600,400))
-    pygame.draw.line(screen, (0,0,0), (200,0), (200,600))
-    pygame.draw.line(screen, (0,0,0), (400,0), (400,600))
+    pygame.draw.line(screen, (0,0,0), (0,height/3), (width,height/3))
+    pygame.draw.line(screen, (0,0,0), (0,2*height/3), (width,2*height/3))
+    pygame.draw.line(screen, (0,0,0), (width/3,0), (width/3,height))
+    pygame.draw.line(screen, (0,0,0), (2*width/3,0), (2*width/3,height))
 
-    availableGrid = ['','','','','','','','','','']
-    currentPlayer = 'X'
+    for i in range(len(availableGrid)):
+        availableGrid[i] = ""
+
+    ran=random.randint(0,1)
+    if ran == 0:
+        currentPlayer = 'X'
+    else:
+        currentPlayer = 'O'
+
     running = True
 
     pygame.display.update()
 
-    return screen, availableGrid, currentPlayer, running
 
 def getGrid(mx, my):
-    if mx <= 200:
-        if my <=200:
+    if mx <= width/3:
+        if my <=height/3:
+            return 0
+        elif my <=2*height/3:
             return 1
-        elif my <=400:
+        else:
             return 2
-        else:
+    elif mx <=2*width/3:
+        if my <=height/3:
             return 3
-    elif mx <=400:
-        if my <=200:
+        elif my <=2*height/3:
             return 4
-        elif my <=400:
+        else:
             return 5
-        else:
-            return 6
     else:
-        if my <=200:
+        if my <=height/3:
+            return 6
+        elif my <=2*height/3:
             return 7
-        elif my <=400:
-            return 8
         else:
-            return 9
+            return 8
 
-def drawX(grid, screen):
-    x1 = 20
-    x2 = 180
-    y1 = 20
-    y2 = 180
-    if grid == 1:
+def drawX(grid):
+    global screen
+
+    x1 = int(width/30)
+    x2 = int(width/3 - x1)
+    y1 = int(height/30)
+    y2 = int(height/3 - y1)
+
+    xsize = int(width/3)
+    ysize = int(height/3)
+
+    if grid == 0:
         pass
+    elif grid == 1:
+        y1+=ysize
+        y2+=ysize
     elif grid == 2:
-        y1+=200
-        y2+=200
+        y1+=2*ysize
+        y2+=2*ysize
     elif grid == 3:
-        y1+=400
-        y2+=400
+        x1+=xsize
+        x2+=xsize
     elif grid == 4:
-        x1+=200
-        x2+=200
+        x1+=xsize
+        x2+=xsize
+        y1+=ysize
+        y2+=ysize
     elif grid == 5:
-        x1+=200
-        x2+=200
-        y1+=200
-        y2+=200
-    elif grid ==6:
-        x1+=200
-        x2+=200
-        y1+=400
-        y2+=400
-    elif grid == 7:
-        x1+=400
-        x2+=400
-    elif grid == 8:
-        x1+=400
-        x2+=400
-        y1+=200
-        y2+=200
-    elif grid == 9:
-        x1+=400
-        x2+=400
-        y1+=400
-        y2+=400
-
-    pygame.draw.line(screen, (0,0,0), (x1,y1), (x2,y2))
-    pygame.draw.line(screen, (0,0,0), (x1,y1+1), (x2,y2+1))
-    pygame.draw.line(screen, (0,0,0), (x1+1,y1), (x2+1,y2))
-
-    pygame.draw.line(screen, (0,0,0), (x2,y1), (x1,y2))
-    pygame.draw.line(screen, (0,0,0), (x2+1,y1), (x1+1,y2))
-    pygame.draw.line(screen, (0,0,0), (x2-1,y1), (x1-1,y2))
-
-def drawO(grid, screen):
-    x = 100
-    y = 100
-
-    if grid == 1:
-        pass
-    elif grid == 2:
-        y +=200
-    elif grid == 3:
-        y+=400
-    elif grid == 4:
-        x+=200
-    elif grid == 5:
-        x+=200
-        y+=200
+        x1+=xsize
+        x2+=xsize
+        y1+=2*ysize
+        y2+=2*ysize
     elif grid == 6:
-        x+=200
-        y+=400
+        x1+=2*xsize
+        x2+=2*xsize
     elif grid == 7:
-        x+=400
+        x1+=2*xsize
+        x2+=2*xsize
+        y1+=ysize
+        y2+=ysize
     elif grid == 8:
-        x+=400
-        y+=200
-    elif grid ==9 :
-        x+=400
-        y+=400
+        x1+=2*xsize
+        x2+=2*xsize
+        y1+=2*ysize
+        y2+=2*ysize
+
+
+    pygame.draw.line(screen, (0,0,0), (x1,y1), (x2,y2), 3)
+    #pygame.draw.line(screen, (0,0,0), (x1,y1+1), (x2,y2+1))
+    #pygame.draw.line(screen, (0,0,0), (x1+1,y1), (x2+1,y2))
+
+    pygame.draw.line(screen, (0,0,0), (x2,y1), (x1,y2), 3)
+    #pygame.draw.line(screen, (0,0,0), (x2+1,y1), (x1+1,y2))
+    #pygame.draw.line(screen, (0,0,0), (x2-1,y1), (x1-1,y2))
+
+def drawO(grid):
+    global screen
+
+    x = int(width/6)
+    y = int(height/6)
+
+    xsize = int(width/3)
+    ysize = int(height/3)
+
+    radius = int(x - (xsize/10))
+
+    if grid == 0:
+        pass
+    elif grid == 1:
+        y +=ysize
+    elif grid == 2:
+        y+=2*ysize
+    elif grid == 3:
+        x+=xsize
+    elif grid == 4:
+        x+=xsize
+        y+=ysize
+    elif grid == 5:
+        x+=xsize
+        y+=2*ysize
+    elif grid == 6:
+        x+=2*xsize
+    elif grid == 7:
+        x+=2*xsize
+        y+=ysize
+    elif grid == 8 :
+        x+=2*xsize
+        y+=2*ysize
 
     pygame.draw.circle(screen, (0,0,0), (x,y), 80, 3)
 
 def checkWinner(grid):
 
+    x = width/6
+    y = height/6
+    xs = width/3
+    ys = height/3
+
     #Verical Lines
-    if grid[1] == grid[2] and grid[2] == grid[3] and grid[1] != '':
-        return True, grid[1], [(100, 0), (100, 600)]
-    if grid[4] == grid[5] and grid[5] == grid[6] and grid[4] != '':
-        return True, grid[4], [(300, 0), (300, 600)]
-    if grid[7] == grid[8] and grid[8] == grid[9] and grid[7] != '':
-        return True, grid[7], [(500, 0), (500, 600)]
+    if grid[0] == grid[1] and grid[1] == grid[2] and grid[0] != '':
+        return True, grid[0], [(x, 0), (x, height)]
+    if grid[3] == grid[4] and grid[4] == grid[5] and grid[3] != '':
+        return True, grid[3], [(x+xs, 0), (x+xs, height)]
+    if grid[6] == grid[7] and grid[7] == grid[8] and grid[6] != '':
+        return True, grid[6], [(x+2*xs, 0), (x+2*xs, height)]
 
     #Horizontal Lines
+    if grid[0] == grid[3] and grid[3] == grid[6] and grid[0] != '':
+        return True, grid[0], [(0, y), (width, y)]
     if grid[1] == grid[4] and grid[4] == grid[7] and grid[1] != '':
-        return True, grid[7], [(0, 100), (600, 100)]
+        return True, grid[1], [(0, y+ys), (width, y+ys)]
     if grid[2] == grid[5] and grid[5] == grid[8] and grid[2] != '':
-        return True, grid[2], [(0, 300), (600, 300)]
-    if grid[3] == grid[6] and grid[6] == grid[9] and grid[3] != '':
-        return True, grid[3], [(0, 500), (600, 500)]
+        return True, grid[2], [(0, y+2*ys), (width, y+2*ys)]
 
     #Diagonals
-    if grid[1] == grid[5] and grid[5] == grid[9] and grid[1] != '':
-        return True, grid[5], [(0, 0), (600, 600)]
-    if grid[7] == grid[5] and grid[5] == grid[3] and grid[7] != '':
-        return True, grid[5], [(600, 0), (0, 600)]
+    if grid[0] == grid[4] and grid[4] == grid[8] and grid[0] != '':
+        return True, grid[4], [(0, 0), (width, height)]
+    if grid[6] == grid[4] and grid[4] == grid[2] and grid[6] != '':
+        return True, grid[4], [(width, 0), (0, height)]
 
     return False, '', ''
 
 def main():
-    screen, availableGrid, currentPlayer, running = initializeGame()
+    global screen, availableGrid, currentPlayer, running
+
+    initializeGame()
 
     #Game Loop
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-                pygame.quit()
+                break
             if event.type == pygame.MOUSEBUTTONDOWN:
                 mx, my = pygame.mouse.get_pos()
                 grid = getGrid(mx, my)
                 if availableGrid[grid] == '':
                     if currentPlayer == 'X':
-                        drawX(grid, screen)
+                        drawX(grid)
                         availableGrid[grid] = 'X'
                         currentPlayer = 'O'
                     else:
-                        drawO(grid, screen)
+                        drawO(grid)
                         availableGrid[grid] = 'O'
                         currentPlayer = 'X'
 
@@ -182,6 +201,29 @@ def main():
                         pygame.draw.line(screen, (255,0,0), strikeThrough[0], strikeThrough[1], 3)
             pygame.display.update()
 
+pygame.init()
 
+width = 600
+height = 600
+
+screen = pygame.display.set_mode((width, height))
+
+#Set caption
+pygame.display.set_caption("Tic Tac Toe")
+
+#Set Icon
+icon = pygame.image.load('tic-tac-toe.png')
+pygame.display.set_icon(icon)
+
+availableGrid = ['','','','','','','','','']
+
+#Deciding player 1 at random
+ran=random.randint(0,1)
+if ran == 0:
+    currentPlayer = 'X'
+else:
+    currentPlayer = 'O'
+
+running = True
 
 main()
